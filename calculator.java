c3e8ed9899e0d40b677fe9e50b39faed23ba8b56
danviper001 <danieldavidviper001@gmail.com>
2024-11-01 12:10:02 +0100

    Added addition method to handle decimal

--- calculator.java
+++ calculator.java
@@ -1,1 +1,12 @@
-public class calculator{																							public int addition(int a, int b){																					return a + b;																						}																						}
+public class Calculator{
+
+	public int addition(int a, int b){
+		return a+b;
+
+	}
+
+	public double addition(int a, int b,){
+		return a+b
+	}
+
+}
